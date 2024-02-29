4c11567b44891b3274db90ad63ab3f5d8975b21a
patrck269 <65741387+patrck269@users.noreply.github.com>
2024-02-28 22:13:40 -0500

    Update shipgpsserver.lua

--- Server/shipgpsserver.lua
+++ Server/shipgpsserver.lua
@@ -9,7 +9,9 @@
         print("X: " .. pos.x)
         print("Y: " .. pos.y)
         print("Z: " .. pos.z)
-        rednet.broadcast("X: " .. pos.x .. " Y: " .. pos.y .. " Z: " .. pos.z,"shiplocreply")
+        rednet.broadcast("X: " .. pos.x .. [[
+            Y: ]] .. pos.y .. [[
+            Z: ]] .. pos.z,"shiplocreply")
 
 
     end
